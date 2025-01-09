c915d2ee052aa8e51c03dcee90a6ef2729980b6a
RKRoberts97 <rkroberts97@gmail.com>
2025-01-09 15:04:15 -0700

    adding to the readme

---
+++ notes/week1/notes_thur.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Data types
+        int x = 9;
+        float gpa = 6.7;
+        double gpa2 = 6.347344;
+        string wowza = "yep";
+        bool statement = false;
+        Console.WriteLine($"did you cry? {wowza}");
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x=9;
+        Console.WriteLine(x +9);
         Console.WriteLine("Hello Brother Comau!");
     }
 }
