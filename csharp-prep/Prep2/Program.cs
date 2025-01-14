7f99c693062b004cd03c1b6c23be099f8b859b72
RKRoberts97 <rkroberts97@gmail.com>
2025-01-13 22:50:25 -0700

    hw

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score? ");
+        int score = int.Parse(Console.ReadLine()); // Parse input to an integer
+        char grade;
+
+        if (score >= 90)
+        {
+            grade = 'A';
+        }
+        else if (score >= 80)
+        {
+            grade = 'B';
+        }
+        else if (score >= 70)
+        {
+            grade = 'C';
+        }
+        else if (score >= 60)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'F';
+        }
+
+        Console.WriteLine($"You got a {grade}");
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! You can do it.");
+        }
     }
 }
